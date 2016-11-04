04f058d708b294102429c7ac5d7f4e4189d9ed8a
Dhananjay Pandit <djpandit007@users.noreply.github.com>
2016-11-04 12:13:21 -0700

    Add files via upload

---
+++ cucumber-intro/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CucumberIntro::Application.config.secret_token = 'df7847fb732fe36931f45729ca9585f261ab94195349995fb54f2bd348c1095328ec0b92f7dd29731d1b677280c263c62d47f9eb9ce35ebc5774fa4014850e18'
